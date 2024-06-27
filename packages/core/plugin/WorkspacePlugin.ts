e1f913b18ecb9025b956f4512c76255395ba7d26
Binu Varghese <binsdevapps@gmail.com>
2024-06-27 18:19:50 +1200

    feat(erase): tested erase feature from another pr and its become a draw actually

--- packages/core/plugin/WorkspacePlugin.ts
+++ packages/core/plugin/WorkspacePlugin.ts
@@ -79,6 +79,7 @@ class WorkspacePlugin implements IPluginTempl {
       height,
       id: 'workspace',
       strokeWidth: 0,
+      erasable: false,
     });
     workspace.set('selectable', false);
     workspace.set('hasControls', false);
